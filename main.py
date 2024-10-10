2bf28373f4e4a412254fb0684d58dd7068a1d6cc
Thereal-Vadim <126063869+Thereal-Vadim@users.noreply.github.com>
2024-10-10 19:38:21 +0200

    add-new-const

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Game Target')
+icon = pygame.image.load('icon.png')
+
+
 running = True
 
 while running:
