fa30136545dffcb2fc71a128c8db426519e2b3e9
Thereal-Vadim <126063869+Thereal-Vadim@users.noreply.github.com>
2024-10-10 20:22:31 +0200

    full-const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,17 @@ SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Game Target')
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load('game_icon.png')
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load('target_images.png')
+target_width = 50
+target_height = 50
+
+target_x = random.randint (0, SCREEN_WIDTH - target_width)
+target_y = random.randint (0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint (0, 255), random.randint (0, 255), random.randint (0, 255))
 
 
 running = True
